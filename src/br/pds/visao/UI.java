de68850e462434e54ba3da9a32f1d0c0b21655b7
Clara Nobre <aclaranobre@gmail.com>
2017-09-06 16:50:02 -0300

    JPanel para o menu adicionado

--- src/br/pds/visao/UI.java
+++ src/br/pds/visao/UI.java
@@ -3,78 +3,67 @@ package br.pds.visao;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class UI {
+public class UI extends JFrame implements ActionListener {
 	private JFrame janela;
-	private JFrame sjanela;
-	private Container painelPrincipal;
+	private JFrame jfeed;
 	
-	/**
-	 * Main e MontaTela
-	 */
-
-	private void preparaJanela() {
-		janela = new JFrame("Meu Socio");
-		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	public UI() {
 		
-		sjanela = new JFrame("Feed");
-		sjanela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-
-	private void mostraJanela() {
+		super("Meu Sócio");
+		
+		janela = new JFrame("Feed");
 		janela.pack();
-		//janela.setLayout(new FlowLayout(FlowLayout.CENTER));
 		janela.setSize(540, 540);
-		janela.setVisible(true);
-
-		sjanela.pack();
-		sjanela.setSize(640, 400);
 		
-	}
-
-	public static void main(String[] args) {
-		new UI().montaTela();
-	}
-
-	private void montaTela(){
-		preparaJanela();
-		preparaPainelPrincipal();
-		preparaPainelBotoes();
-		preparaBotao();
-		mostraJanela();
-	}
-	
-	private void preparaPainelPrincipal() {
-		painelPrincipal = new JPanel();
-		painelPrincipal.setLayout(new );
-		janela.add(painelPrincipal);
-	}
-	
-	private void preparaPainelBotoes() {
-		new JPanel(new GridLayout());
-	}
-
-	private void preparaBotao() {
+		Container c = getContentPane();
+		//c.setLayout(new BorderLayout(15,5));
+		
 		JButton botaoEntrar = new JButton("Entrar");
-		botaoEntrar.setBorder(BorderFactory.createRaisedBevelBorder());
+		getContentPane().add(botaoEntrar);
 		botaoEntrar.setBackground(Color.WHITE);
-		//botaoEntrar.setBounds(130,100,270,400);
-		
+		c.add(BorderLayout.SOUTH, botaoEntrar);
 		botaoEntrar.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				sjanela.setVisible(true);
+				janela.setVisible(true);
 			}
 		});
-		painelPrincipal.add(botaoEntrar, BorderLayout.SOUTH);
+		
+		Container c2 = new JPanel();
+		c2.setLayout(new GridLayout(4,1));
+		c2.add(new JButton("Menu"));
+		//c.add(BorderLayout.CENTER, new JButton("centro"));
+		//c.add(BorderLayout.EAST, c2);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(640, 400);
+		setVisible(true);
+
+	}
+	
+	/**
+	 * Main
+	 */
+	public static void main(String[] argvs) {
+		new UI();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
+
+/**
+ * JLabel simples = new JLabel("Label Teste");
+ * simples.setToolTipText("Meu Label");
+ */
