d8130febdc855a6a6e39bca3322ef5b4e5d2ce14
Clara Nobre <aclaranobre@gmail.com>
2017-11-21 22:43:39 -0300

    Atualizando as classes da tabela

---
+++ MeuSocio/app/src/main/java/TabelaFirebase.java
@@ -0,0 +1,29 @@
+package DAO;
+
+import android.support.v4.widget.DrawerLayout;
+
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Clara Nobre on 21/11/2017.
+ */
+
+public class TabelaFirebase {
+    int placar;
+    DrawerLayout formatoPlacar;
+    private ArrayList<String> modalidade;
+
+
+   /* private ArrayList<String> verificarModalidade(){
+        return modalidade;
+    }
+
+    private DatabaseReference getModalidadePlacar(){}
+
+    private DrawerLayout onCreatePlacar(int formatoPlacar){}
+
+    private DatabaseReference atualizarPlacar(){}*/
+
+}
